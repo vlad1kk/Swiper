8fd777bb078a162861abb20ffcafca72d579bbc3
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-03-20 15:49:15 +0200

    type: 'fraction'

--- js/script.js
+++ js/script.js
@@ -8,12 +8,21 @@ new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         // Булети. При кліку на кружечки гортає зображення:
-        clickable: true,
+        // clickable: true,
         // Щоб динамічно змінювались кружечки знизу:
-        dynamicBullets: true,
+        // dynamicBullets: true,
         // Кастомний булет:
-        renderBullet: function(index, className){
-            return'<span class="' + className + '">' + (index + 1) +'</span>';
-        },
+        // renderBullet: function(index, className){
+        //     return'<span class="' + className + '">' + (index + 1) +'</span>';
+        // },
+        // Фракції:
+        // type: 'fraction' - для відображення не булетами(кружечками), а на якому конкретному елементі знаходжусь і через скісну риску з скількох елементів
+        type: 'fraction',
+        //Кастомний вивід фракції:
+        renderFraction: function(currentClass, totalClass){
+            return ' Photo <span class="' + currentClass + '"></span>' + 
+            ' with ' +
+            '<span class="' + totalClass + '"></span>';
+            },
     },
 });
